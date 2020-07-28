aea3f8b6a79df937167b9958f01c0f8bd3fdec82
Lina DAVILA <lina_davila88@hotmail.com>
2020-07-27 21:44:23 -0500

    table creation
    create each table and each row

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+//variable fo tbody
+var tbody = d3.select("tbody");
+
+// Build table
+function buildTable(data) {
+    //clean data
+  tbody.html("");
+
+  // Loop to read and add each record
+  data.forEach((dataRow) => {
+    var row = tbody.append("tr");
+    Object.values(dataRow).forEach((val) => {
+      var cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
