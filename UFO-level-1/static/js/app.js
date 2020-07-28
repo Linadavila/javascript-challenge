8560a684a48b945e7e22ab24d8bb340ea2f2c504
Lina DAVILA <lina_davila88@hotmail.com>
2020-07-27 22:40:55 -0500

    final result
    final code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,15 +2,17 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+//INITIAL TABLE
 //variable fo tbody
 var tbody = d3.select("tbody");
 
-// Build table
+//build initial table
 function buildTable(data) {
     //clean data
   tbody.html("");
 
-  // Loop to read and add each record
+  //loop to read and add each record
   data.forEach((dataRow) => {
     var row = tbody.append("tr");
     Object.values(dataRow).forEach((val) => {
@@ -19,4 +21,42 @@ function buildTable(data) {
     });
   });
 }
+
+buildTable(tableData);
+
+
+
+//FILTERED TABLE
+//create variable to store date to filter
+var filters = {};
+
+//get input value from filter and add it to the filter variable
+function DateFilter() {
+  var changedElement = d3.select(this).select("input");
+  var elementValue = changedElement.property("value");
+  var filterId = changedElement.attr("id");
+  filters[filterId] = elementValue;
+
+  //call function for filtered table
+  FilteredTable();
+}
+
+
+function FilteredTable() {
+  //assign filtred data
+  let filteredData = tableData;
+
+  //loop to read and add each filtered value
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+
+  //create filtered table
+  buildTable(filteredData);
+}
+
+
+
+// MAKE D3 WORK
+d3.selectAll(".filter").on("change", DateFilter);
 
